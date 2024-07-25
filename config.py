1e6544007850f47eadae9113fa7103d8fdb4171a
Ra1nek <146630228+Ra1nek@users.noreply.github.com>
2024-07-25 17:05:48 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Константы для путей
+RESOURCES_PATH = 'resources/'
+FONTS_PATH = RESOURCES_PATH + 'fonts/'
+STYLES_PATH = RESOURCES_PATH + 'style.qss'
+
+# Константы для логгирования
+LOGGING_LEVEL = 'INFO'
+LOGGING_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
